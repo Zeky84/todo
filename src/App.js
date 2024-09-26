0dd4ca2e5b32413e443523ba4fd403648903f58a
Ezequiel <97308027+Zeky84@users.noreply.github.com>
2024-09-26 11:30:01 -0400

    another commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './Web/Homepage/homepage';
+import Dashboard from './Web/Dashboard/dashboard';
+import Errorpage from './Web/Errorpage/errorpage';
 
 
 
@@ -9,7 +11,8 @@ function App() {
   return (<Router>
     <Routes>
       <Route path='/' element={<Homepage firstName={firstName} />}/>
-      <Route path='/'></Route>
+      <Route path='/dashboard' element={<Dashboard/>}></Route>
+      <Route path='*' element={<Errorpage/>}></Route>
     </Routes>
   </Router>
     
