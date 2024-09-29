b35fee00ccf7fe26308cd3a425d4cc6529541662
Ezequiel <97308027+Zeky84@users.noreply.github.com>
2024-09-28 20:14:39 -0400

    more commits

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
     return (
         <div>
-            <h1>Header</h1>
+            <h1>Todo App</h1>
         </div>
     );
 };
--- src/Web/Homepage/homepage.js
+++ src/Web/Homepage/homepage.js
@@ -1,33 +1,58 @@
-import React, { useEffect,useState } from 'react';
+// import React, { useEffect, useState } from 'react';
 import Header from '../../Components/Header/header';
-import AnotherHeader, { AnotherHeaderOn } from '../../Components/Header/anotherHeader';
+// import AnotherHeader, { AnotherHeaderOn } from '../../Components/Header/anotherHeader';
 import Footer from '../../Components/Footer/footer';
 
 const Homepage = () => {
+    const todoItems = [
+        { id:1, itemName: "get milk", isDone: true },
+        { id:2, itemName: "get bread", isDone: false },
+        { id:3, itemName: "get eggs", isDone: false },
+    ];
 
-    const [name, setName] = useState("Manolo");//getter(name) and setter(setName) the same that java but less verbose 
-    const [age, setAge] = useState(0);
-    const [lastName, setLastName] = useState("Lopez");
+    // const [name, setName] = useState("Manolo");//getter(name) and setter(setName) the same that java but less verbose 
+    // const [lastName, setLastName] = useState("Lopez");
+    // const [age, setAge] = useState(20);
 
-    useEffect( () => {
-        console.log("hello my name is " + name);
-    }, [name]);
-    
-    
+    // useEffect(() => {
+    //     console.log("My Name is " + name);
+    // }, [name]);
+
+    // useEffect(() => {
+    //     console.log("My Last Name i is " + lastName);
+    // }, [lastName]);
+
+
 
     return (
         <div className="homepage">
-            <Header />
-            <div>
+            {/* <div>
                 <label htmlFor="name" style={{ color: "grey", marginRight: "10px" }}>First Name:</label>
-                <input type="text" id="setName" onChange={(e) =>setName(e.target.value)} value= {name}/>
-            </div>
-            <br />
-            <div>
-                {name}
-            </div>
-            <AnotherHeader />
-            <AnotherHeaderOn />
+                <input type="text" id="name" onChange={(e) =>setName(e.target.value)} value= {name}/>
+                </div>
+                <div>
+                <label htmlFor="lastName" style={{ color: "grey", marginRight: "10px" }}>Last Name:</label>
+                <input type="text" id="lastName" onChange={(e) =>setLastName(e.target.value)} value= {lastName}/>
+                </div>
+                <div>
+                <label htmlFor="age" style={{ color: "grey", marginRight: "10px" }}>age:</label>
+                <input type="number" id="age" onChange={function (e){
+                    console.log(e);//printing the event to see all the things it fires
+                    setAge(e.target.ariaValueNow)}} value= {age}/>
+                    </div>
+                    <br />
+                    <div>
+                    My name is  {name} {lastName} and I am {age} years old
+                    </div> */}
+            <Header />
+            <h3>Todo list</h3>
+            {todoItems.map((todoItem) => {
+                return (
+                    <div key={todoItem.id}>
+                        <label style={ todoItem.isDone===true ? {textDecoration: "line-through"}:{}}> {todoItem.itemName}</label>
+                    </div>
+                );
+            })}
             <Footer />
         </div>
     );
